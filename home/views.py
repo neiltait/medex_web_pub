afa4c1ac259573b0f74d6092f1ab32638d21f56f
Matt Nicks <matt.nicks@methods.co.uk>
2019-01-30 15:13:37 +0000

    add some initial endpoints

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+  return HttpResponse('Your current open cases')
+
+def login(request):
+  return HttpResponse('Login to access your cases')
