86102c547c0fd817310a488b1f2418f5a514e7d3
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-13 17:37:29 +0000

    create forgotten creds form classes

--- home/forms.py
+++ home/forms.py
@@ -1,9 +1,9 @@
 
 class LoginForm():
 
-  def __init__(self, response):
-    self.user_id = response.get('user_id')
-    self.password = response.get('password')
+  def __init__(self, request):
+    self.user_id = request.get('user_id')
+    self.password = request.get('password')
 
   def is_valid(self):
     return True if self.user_id and self.password else False
@@ -13,3 +13,23 @@ class LoginForm():
     # Temporary auth check until we have OCTA integrated
     # may need to add in an attempt check if OCTA doesn't have one.
     return self.user_id == 'Matt' and self.password == 'Password'
+
+
+class ForgottenPasswordForm():
+
+  def __init__(self, request):
+    self.user_id = request.get('user_id')
+
+
+  def is_valid(self):
+    return True if self.user_id else False
+
+
+class ForgottenUserIdForm():
+
+  def __init__(self, request):
+    self.email_address = request.get('email_address')
+
+
+  def is_valid(self):
+    return True if self.email_address else False
