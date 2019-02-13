b5d26d423cc76cc64f40010dab5add27a1706873
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-13 20:33:25 +0000

    move error messages in to alerts app

---
+++ alerts/messages.py
@@ -0,0 +1,3 @@
+INVALID_CREDENTIALS = 'Invalid User ID and/or Password entered'
+
+MISSING_CREDENTIALS = 'Please enter a User ID and Password'
--- errors/messages.py
+++
@@ -1,5 +0,0 @@
-def invalid_credentials():
-  return 'Invalid User ID and/or Password entered'
-
-def missing_credentials():
-  return 'Please enter a User ID and Password'
