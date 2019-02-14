a59d26928ada6f84ddcf94bd5a3762e4b285c80b
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-14 13:06:32 +0000

    implement initial lookup post functionality

--- users/views.py
+++ users/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from rest_framework import status
+
+from alerts import messages
+from alerts.utils import generate_error_alert
+
+from .forms import UserLookupForm
 
 def user_lookup(request):
   context = {
@@ -8,6 +15,15 @@ def user_lookup(request):
     },
   }
   alerts = []
+  status_code = status.HTTP_200_OK
+
+  if (request.POST):
+    form = UserLookupForm(request.POST)
+    if (form.is_valid()):
+      return redirect('/users/TestUser')
+    else:
+      alerts.append(generate_error_alert(messages.MISSING_EMAIL))
+      status_code = status.HTTP_400_BAD_REQUEST
 
   context['alerts'] = alerts
-  return render(request, 'users/lookup.html', context)
+  return render(request, 'users/lookup.html', context, status=status_code)
