43dba955c4eec884ee591e03164d6b3ae5e7ccfc
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-14 12:46:01 +0000

    build lookup form class

---
+++ users/forms.py
@@ -0,0 +1,9 @@
+
+class UserLookupForm():
+
+  def __init__(self, request):
+    self.email_address = request.get('email_address')
+
+  def is_valid(self):
+    return True if self.email_address else False
+    
