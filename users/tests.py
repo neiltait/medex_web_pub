126a22c5232dbe17d744ff21b0e650a993e6733c
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-14 10:39:55 +0000

    write test for loading user lookup

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from medexCms.test.utils import MedExTestCase
 
-# Create your tests here.
+class UsersViewsTest(MedExTestCase):
+
+  def test_landing_on_the_user_lookup_page_loads_the_correct_template_with_empty_context(self):
+    response = self.client.get('/users/lookup')
+    self.assertTemplateUsed(response, 'users/lookup.html')
+    alert_list = self.get_context_value(response.context, 'alerts')
+    self.assertEqual(len(alert_list), 0)
