ca43e735620a26d09f38c0e31a45aa93bf40bb97
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-20 15:15:35 +0000

    remove unneccessary message

--- alerts/messages.py
+++ alerts/messages.py
@@ -8,6 +8,4 @@ MISSING_EMAIL = 'Please enter an email address'
 
 FORGOTTEN_PASSWORD_SENT = 'If the user ID provided is valid an email has been sent to help you reset your password'
 
-FORGOTTEN_ID_SENT = 'If the email address provided belongs to an account you have been sent an email to remind you of your user ID'
-
 OBJECT_NOT_FOUND = 'The %s requested could not be found'
