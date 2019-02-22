aed28e3c53232a30ddd12755ededbd08a13a531d
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-22 09:13:30 +0000

    build out initial create case endpoint

--- examinations/views.py
+++ examinations/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from home.utils import check_logged_in, redirect_to_login
+
+def create_examination(request):
+  if not check_logged_in(request):
+    return redirect_to_login()
+
+  context = {}
+  alerts = []
+
+  return render(request, 'examinations/create.html', context)
