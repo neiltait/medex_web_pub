08e0e678674f1469e5046f7fc52700f9a5981ad7
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-26 22:19:48 +0000

    add user and trusts to context

--- cms/users/views.py
+++ cms/users/views.py
@@ -5,18 +5,26 @@ from rest_framework import status
 from alerts import messages
 from alerts.utils import generate_error_alert
 
+from locations.request_handler import load_trusts_list
+
 from home.utils import redirect_to_login
 
 from .models import User
 
 
 def create_user(request):
-  context = {}
   user = User.initialise_with_token(request)
 
   if not user.check_logged_in():
     return redirect_to_login()
 
+  trust_list = load_trusts_list()
+
+  context = {
+    'session_user': user,
+    'page_heading': 'Add a user',
+    'trusts': trust_list
+  }
   alerts = []
   status_code = status.HTTP_200_OK
 
