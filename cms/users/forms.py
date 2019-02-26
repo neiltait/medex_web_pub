62468b7e693e5cb0bf9bfc5a1475581f19514243
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-26 15:47:34 +0000

    remove unneeded code

--- cms/users/forms.py
+++ cms/users/forms.py
@@ -1,8 +1,1 @@
 
-class UserLookupForm():
-
-  def __init__(self, request):
-    self.email_address = request.get('email_address').lower()
-
-  def is_valid(self):
-    return True if self.email_address else False
