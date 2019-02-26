c16693865a837118a63ba5cbb4fda7ed36bdfdaf
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-26 22:14:41 +0000

    add authorisation to the new user endpoint

--- cms/users/views.py
+++ cms/users/views.py
@@ -5,11 +5,18 @@ from rest_framework import status
 from alerts import messages
 from alerts.utils import generate_error_alert
 
+from home.utils import redirect_to_login
+
 from .models import User
 
 
 def create_user(request):
   context = {}
+  user = User.initialise_with_token(request)
+
+  if not user.check_logged_in():
+    return redirect_to_login()
+
   alerts = []
   status_code = status.HTTP_200_OK
 
