e67d975bc4113662b5b2919b0554152880484d83
Matt Nicks <matt.nicks@methods.co.uk>
2019-02-27 12:43:59 +0000

    send new user details to api creation end point

--- cms/users/request_handler.py
+++ cms/users/request_handler.py
@@ -4,3 +4,6 @@ import requests
 
 def validate_session(cookie):
   return requests.post('%s/users/validate-session' % settings.API_URL, data = {'auth_token': cookie})
+
+def create_user(user_object):
+  return requests.post('%s/users' % settings.API_URL, data = user_object)
--- cms/users/views.py
+++ cms/users/views.py
@@ -9,6 +9,7 @@ from locations.request_handler import load_trusts_list
 
 from home.utils import redirect_to_landing, redirect_to_login
 
+from . import request_handler
 from .forms import CreateUserForm
 from .models import User
 
@@ -34,12 +35,20 @@ def create_user(request):
     form = CreateUserForm(request.POST)
 
     if form.validate():
-      return redirect_to_landing()
+      response = request_handler.create_user({'email': form.email_address})
+
+      if response.status_code == status.HTTP_200_OK:
+        print(response.json()['id'])
+        return redirect_to_landing()
+      else:
+        alerts.append(generate_error_alert(messages.ERROR_IN_FORM))
+        status_code = response.status_code
     else:
       alerts.append(generate_error_alert(messages.ERROR_IN_FORM))
       status_code = status.HTTP_400_BAD_REQUEST
-      context['form'] = form
-      context['invalid'] = True
+
+    context['form'] = form
+    context['invalid'] = True
 
   context['alerts'] = alerts
   return render(request, 'users/new.html', context, status=status_code)
