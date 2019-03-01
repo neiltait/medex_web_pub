0a515a2c8d22937bec6fcde44953b4ad81e3df8e
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2019-03-01 11:12:54 +0000

    fix failing tests by patching endpoints

--- cms/examinations/tests.py
+++ cms/examinations/tests.py
@@ -1,18 +1,52 @@
+import json
+from unittest.mock import patch
+
 from django.conf import settings
 
 from http.cookies import SimpleCookie
 
+from requests import Response
 from rest_framework import status
 
 import uuid
 
 from medexCms.test.utils import MedExTestCase
 
+locations = [{
+    'id': '1',
+    'name': 'Barnet Hospital',
+  }, {
+    'id': '2',
+    'name': 'Sheffield Hospital',
+  }, {
+    'id': '3',
+    'name': 'Gloucester Hospital',
+  }]
+GET_LOCATIONS_RESPONSE = Response()
+GET_LOCATIONS_RESPONSE.status_code = status.HTTP_200_OK
+GET_LOCATIONS_RESPONSE._content = json.dumps(locations).encode('utf-8')
+
+offices = [{
+        'id': '1',
+        'name': 'Barnet Hospital ME Office',
+    }, {
+        'id': '2',
+        'name': 'Sheffield Hospital ME Office',
+    }, {
+        'id': '3',
+        'name': 'Gloucester Hospital ME Office',
+    }]
+GET_ME_OFFICES_RESPONSE = Response()
+GET_ME_OFFICES_RESPONSE.status_code = status.HTTP_200_OK
+GET_ME_OFFICES_RESPONSE._content = json.dumps(offices).encode('utf-8')
+
 class ExaminationsViewsTests(MedExTestCase):
 
   #### Create case tests
 
-  def test_landing_on_create_case_page_loads_the_correct_template(self):
+  @patch('examinations.request_handler.get_locations_list', return_value=locations)
+  @patch('examinations.request_handler.get_me_offices_list', return_value=offices)
+  def test_landing_on_create_case_page_loads_the_correct_template(self, mock_locations, mock_me_offices):
     self.client.cookies = SimpleCookie({settings.AUTH_TOKEN_NAME: uuid.uuid4()})
     response = self.client.get('/cases/create')
     self.assertEqual(response.status_code, status.HTTP_200_OK)
