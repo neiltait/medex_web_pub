52e6cc520bbe02d597c6de9ed129e7e0d879ffb3
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-05 15:05:07 +0000

    add tests for form validation method

--- cms/users/tests.py
+++ cms/users/tests.py
@@ -14,7 +14,7 @@ import json, uuid
 from alerts import utils, messages
 
 from .models import User
-from .forms import CreateUserForm
+from .forms import CreateUserForm, PermissionBuilderForm
 
 from locations import request_handler
 
@@ -162,6 +162,114 @@ class UsersFormsTests(MedExTestCase):
     create_form = CreateUserForm({'email_address': email_address})
     self.assertIsFalse(create_form.check_is_nhs_email())
 
+
+#### PermissionBuilderForm tests
+
+  def test_is_valid_returns_false_when_no_role_given(self):
+    form_content = {
+      'role': None,
+      'permission_level': 'national',
+      'region': None,
+      'trust': None
+    }
+    form = PermissionBuilderForm(form_content)
+    result = form.is_valid()
+    self.assertIsFalse(result)
+    self.assertEqual(form.role_error, messages.FIELD_MISSING % "a role")
+
+  def test_is_valid_returns_false_when_no_level_given(self):
+    form_content = {
+      'role': 'me',
+      'permission_level': None,
+      'region': None,
+      'trust': None
+    }
+    form = PermissionBuilderForm(form_content)
+    result = form.is_valid()
+    self.assertIsFalse(result)
+    self.assertEqual(form.permission_level_error, messages.FIELD_MISSING % "a level")
+
+  def test_is_valid_returns_false_when_no_trust_given_for_a_trust_level_permission(self):
+    form_content = {
+      'role': 'me',
+      'permission_level': 'trust',
+      'region': None,
+      'trust': None
+    }
+    form = PermissionBuilderForm(form_content)
+    result = form.is_valid()
+    self.assertIsFalse(result)
+    self.assertEqual(form.trust_error, messages.FIELD_MISSING % "a trust")
+
+  def test_is_valid_returns_false_when_no_region_given_for_a_region_level_permission(self):
+    form_content = {
+      'role': 'me',
+      'permission_level': 'regional',
+      'region': None,
+      'trust': None
+    }
+    form = PermissionBuilderForm(form_content)
+    result = form.is_valid()
+    self.assertIsFalse(result)
+    self.assertEqual(form.region_error, messages.FIELD_MISSING % "a region")
+
+  def test_is_valid_returns_false_when_no_role_or_level_given(self):
+    form_content = {
+      'role': None,
+      'permission_level': None,
+      'region': None,
+      'trust': None
+    }
+    form = PermissionBuilderForm(form_content)
+    result = form.is_valid()
+    self.assertIsFalse(result)
+    self.assertEqual(form.role_error, messages.FIELD_MISSING % "a role")
+    self.assertEqual(form.permission_level_error, messages.FIELD_MISSING % "a level")
+
+  def test_is_valid_returns_true_when_role_at_national_level_given_with_no_locations(self):
+    form_content = {
+      'role': 'me',
+      'permission_level': 'national',
+      'region': None,
+      'trust': None
+    }
+    form = PermissionBuilderForm(form_content)
+    result = form.is_valid()
+    self.assertIsTrue(result)
+    self.assertEqual(form.role_error, None)
+    self.assertEqual(form.permission_level_error, None)
+    self.assertEqual(form.region_error, None)
+    self.assertEqual(form.trust_error, None)
+
+  def test_is_valid_returns_true_when_role_at_regional_level_given_with_region_given(self):
+    form_content = {
+      'role': 'meo',
+      'permission_level': 'regional',
+      'region': '1',
+      'trust': None
+    }
+    form = PermissionBuilderForm(form_content)
+    result = form.is_valid()
+    self.assertIsTrue(result)
+    self.assertEqual(form.role_error, None)
+    self.assertEqual(form.permission_level_error, None)
+    self.assertEqual(form.region_error, None)
+    self.assertEqual(form.trust_error, None)
+
+  def test_is_valid_returns_true_when_role_at_trust_level_given_with_trust_given(self):
+    form_content = {
+      'role': 'sa',
+      'permission_level': 'trust',
+      'region': None,
+      'trust': '1'
+    }
+    form = PermissionBuilderForm(form_content)
+    result = form.is_valid()
+    self.assertIsTrue(result)
+    self.assertEqual(form.role_error, None)
+    self.assertEqual(form.permission_level_error, None)
+    self.assertEqual(form.region_error, None)
+    self.assertEqual(form.trust_error, None)
 
 
 class UsersModelsTests(MedExTestCase):
