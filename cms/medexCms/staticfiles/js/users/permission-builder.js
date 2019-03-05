f85b9db365cb0bdcfda8346402b849e0a663c19e
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-05 09:37:32 +0000

    add interactivity to permission builder form

---
+++ cms/medexCms/staticfiles/js/users/permission-builder.js
@@ -0,0 +1,69 @@
+(function($){
+
+  var PermissionBuilderForm = function(form) {
+    this.form = $(form);
+    this.setup();
+  }
+
+  PermissionBuilderForm.prototype = {
+    setup: function() {
+      this.roleRadios = new RadioGroup(this.form.find('#role-radio-buttons'), this.handleRoleChange.bind(this));
+      this.levelRadios = new RadioGroup(this.form.find('#level-radio-buttons'), this.handleLevelChange.bind(this));
+      this.regionSelector = this.form.find('#region-selector');
+      this.trustSelector = this.form.find('#trust-selector');
+    },
+
+    handleRoleChange: function(target) {
+      this.levelRadios.show();
+    },
+
+    handleLevelChange: function(target) {
+      if (target.value === 'regional') {
+        this.trustSelector.hide();
+        this.regionSelector.show();
+      } else if (target.value === 'trust') {
+        this.trustSelector.show();
+        this.regionSelector.hide();
+      } else {
+        this.trustSelector.hide();
+        this.regionSelector.hide();
+      }
+    }
+  }
+
+  var RadioGroup = function(wrapper, changeCallback) {
+    this.wrapper = wrapper;
+    this.changeCallback = changeCallback;
+    this.setup();
+  }
+
+  RadioGroup.prototype = {
+    setup: function() {
+      this.radios = this.wrapper.find('input[type=radio]');
+      this.startWatchers();
+    },
+
+    startWatchers: function() {
+      var that = this;
+      for (var i = 0; i < this.radios.length; i++) {
+        $(this.radios[i]).change(function(e) {
+          that.changeCallback(e.target);
+        });
+      }
+    },
+
+    show: function() {
+      this.wrapper.show();
+    }
+  }
+
+  function init(){
+    var permissionForms = $('#permission-builder-form');
+    for (var i = 0; i < permissionForms.length; i++) {
+      new PermissionBuilderForm(permissionForms[i]);
+    }
+  }
+
+  $(document).on('page:load', init);
+  $(init)
+}(jQuery))
