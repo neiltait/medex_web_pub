f486a607e748a6d88719a90ee5db374777af087b
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2019-03-06 19:23:55 +0000

    fixed tests to work with new version of form validation

--- cms/examinations/tests.py
+++ cms/examinations/tests.py
@@ -63,7 +63,7 @@ class ExaminationsViewsTests(MedExTestCase):
         form = PrimaryExaminationInformationForm(request={'data': 'test'})
         result = form.is_valid()
         self.assertIsFalse(result)
-        self.assertEqual(form.first_name_error, ErrorFieldRequiredMessage('first name'))
+        self.assertEqual(form.errors["first_name"], ErrorFieldRequiredMessage('first name'))
 
     def test_given_create_examination_with_first_name_submitted_does_validate(self):
         form = PrimaryExaminationInformationForm(request={'first_name': 'matt'})
@@ -74,23 +74,23 @@ class ExaminationsViewsTests(MedExTestCase):
         form = PrimaryExaminationInformationForm(request={'test': 'data'})
         result = form.is_valid()
         self.assertIsFalse(result)
-        self.assertEqual(form.last_name_error, ErrorFieldRequiredMessage('last name'))
+        self.assertEqual(form.errors["last_name"], ErrorFieldRequiredMessage('last name'))
 
     def test_given_create_examination_with_last_name_submitted_does_validate(self):
         form = PrimaryExaminationInformationForm(request={'last_name': 'nicks'})
         form.is_valid()
-        self.assertIsNone(form.last_name_error)
+        self.assertIsFalse("last_name" in form.errors)
+
 
     def test_given_create_examination_without_gender_when_submitted_does_not_validate(self):
         form = PrimaryExaminationInformationForm(request={'test': 'data'})
         result = form.is_valid()
-        self.assertIsFalse(result)
-        self.assertEqual(form.gender_error, ErrorFieldRequiredMessage('gender'))
+        self.assertEqual(form.errors["gender"], ErrorFieldRequiredMessage('gender'))
 
     def test_given_create_examination_with_gender_submitted_does_validate(self):
         form = PrimaryExaminationInformationForm(request={'gender': 'male'})
         form.is_valid()
-        self.assertIsNone(form.gender_error)
+        self.assertIsFalse("gender" in form.errors)
 
     def test_text_and_checkbox_group_validates_if_checkbox_is_ticked(self):
         # Given
@@ -143,95 +143,93 @@ class ExaminationsViewsTests(MedExTestCase):
     def test_nhs_number_group_does_validate_if_checkbox_ticked(self):
         form = PrimaryExaminationInformationForm({'nhs_number': '', 'nhs_number_not_known': True})
         form.is_valid()
-        self.assertIsNone(form.nhs_number_error)
+        self.assertIsFalse("nhs_number" in form.errors)
 
     def test_nhs_number_group_does_validate_if_text_is_entered(self):
         form = PrimaryExaminationInformationForm({'nhs_number': 'ABC123', 'nhs_number_not_known': False})
         form.is_valid()
-        self.assertIsNone(form.nhs_number_error)
+        self.assertIsFalse("nhs_number" in form.errors)
 
     def test_nhs_number_group_does_not_validate_if_no_information_entered(self):
         form = PrimaryExaminationInformationForm({'nhs_number': '', 'nhs_number_not_known': False})
         form.is_valid()
-        self.assertEqual(form.nhs_number_error, ErrorFieldRequiredMessage('NHS number'))
-
+        self.assertEqual(form.errors["nhs_number"], ErrorFieldRequiredMessage('NHS number'))
 
     def test_time_of_death_group_does_validate_if_checkbox_ticked(self):
         form = PrimaryExaminationInformationForm({'time_of_death': '', 'time_of_death_not_known': True})
         form.is_valid()
-        self.assertIsNone(form.time_of_death_error)
+        self.assertIsFalse("time_of_death" in form.errors)
 
     def test_time_of_death_group_does_validate_if_text_is_entered(self):
         form = PrimaryExaminationInformationForm({'time_of_death': 'ABC123', 'time_of_death_not_known': False})
         form.is_valid()
-        self.assertIsNone(form.time_of_death_error)
+        self.assertIsFalse("time_of_death" in form.errors)
 
     def test_time_of_death_group_does_not_validate_if_no_information_entered(self):
         form = PrimaryExaminationInformationForm({'time_of_death': '', 'time_of_death_not_known': False})
         form.is_valid()
-        self.assertEqual(form.time_of_death_error, ErrorFieldRequiredMessage('time of death'))
-
+        self.assertEqual(form.errors["time_of_death"], ErrorFieldRequiredMessage('time of death'))
 
     def test_date_of_birth_group_does_validate_if_checkbox_ticked(self):
         form = PrimaryExaminationInformationForm({'day_of_birth': '', 'month_of_birth': '', 'year_of_birth': '', 'date_of_birth_not_known': True})
         form.is_valid()
-        self.assertIsNone(form.date_of_birth_error)
+        self.assertIsFalse("day_of_birth" in form.errors)
 
     def test_date_of_birth_group_does_validate_if_all_date_boxes_are_filled(self):
         form = PrimaryExaminationInformationForm({'day_of_birth': '26', 'month_of_birth': '08', 'year_of_birth': '1978', 'date_of_birth_not_known': False})
         form.is_valid()
-        self.assertIsNone(form.date_of_birth_error)
+        self.assertIsFalse("day_of_birth" in form.errors)
 
     def test_date_of_birth_group_does_not_validate_if_no_information_entered(self):
         form = PrimaryExaminationInformationForm({'day_of_birth': '', 'month_of_birth': '', 'year_of_birth': '', 'date_of_birth_not_known': False})
         form.is_valid()
-        self.assertEqual(form.date_of_birth_error, ErrorFieldRequiredMessage('date of birth'))
+        self.assertEqual(form.errors["date_of_birth"], ErrorFieldRequiredMessage('date of birth'))
 
     def test_date_of_birth_group_does_not_validate_if_partial_information_entered(self):
         form = PrimaryExaminationInformationForm({'day_of_birth': '26', 'month_of_birth': '', 'year_of_birth': '', 'date_of_birth_not_known': False})
         form.is_valid()
-        self.assertEqual(form.date_of_birth_error, ErrorFieldRequiredMessage('date of birth'))
+        self.assertEqual(form.errors["date_of_birth"], ErrorFieldRequiredMessage('date of birth'))
 
 
     def test_date_of_death_group_does_validate_if_checkbox_ticked(self):
         form = PrimaryExaminationInformationForm({'day_of_death': '', 'month_of_death': '', 'year_of_death': '', 'date_of_death_not_known': True})
         form.is_valid()
-        self.assertIsNone(form.date_of_death_error)
+        self.assertIsFalse("date_of_death" in form.errors)
 
     def test_date_of_death_group_does_validate_if_all_date_boxes_are_filled(self):
         form = PrimaryExaminationInformationForm({'day_of_death': '26', 'month_of_death': '08', 'year_of_death': '1978', 'date_of_death_not_known': False})
         form.is_valid()
-        self.assertIsNone(form.date_of_death_error)
+        self.assertIsFalse("date_of_death" in form.errors)
 
     def test_date_of_death_group_does_not_validate_if_no_information_entered(self):
         form = PrimaryExaminationInformationForm({'day_of_death': '', 'month_of_death': '', 'year_of_death': '', 'date_of_death_not_known': False})
         form.is_valid()
-        self.assertEqual(form.date_of_death_error, ErrorFieldRequiredMessage('date of death'))
+        self.assertEqual(form.errors["date_of_death"], ErrorFieldRequiredMessage('date of death'))
 
     def test_date_of_death_group_does_not_validate_if_partial_information_entered(self):
         form = PrimaryExaminationInformationForm({'day_of_death': '26', 'month_of_death': '', 'year_of_death': '', 'date_of_birth_not_known': False})
         form.is_valid()
-        self.assertEqual(form.date_of_death_error, ErrorFieldRequiredMessage('date of death'))
+        self.assertEqual(form.errors["date_of_death"], ErrorFieldRequiredMessage('date of death'))
 
     def test_place_of_death_does_not_validate_if_missing(self):
         form = PrimaryExaminationInformationForm({'test':'data'})
         form.is_valid()
-        self.assertEqual(form.place_of_death_error, ErrorFieldRequiredMessage('place of death'))
+        self.assertEqual(form.errors["place_of_death"], ErrorFieldRequiredMessage('place of death'))
 
     def test_place_of_death_does_validate_if_present(self):
         form = PrimaryExaminationInformationForm({'place_of_death': 1})
         form.is_valid()
-        self.assertIsNone(form.place_of_death_error)
+        self.assertIsFalse("place_of_death" in form.errors)
 
     def test_me_office_does_not_validate_if_missing(self):
         form = PrimaryExaminationInformationForm({'test':'data'})
         form.is_valid()
-        self.assertEqual(form.me_office_error, ErrorFieldRequiredMessage('ME office'))
+        self.assertEqual(form.errors["me_office"], ErrorFieldRequiredMessage('ME office'))
 
     def test_me_office_does_validate_if_present(self):
         form = PrimaryExaminationInformationForm({'me_office': 1})
         form.is_valid()
-        self.assertIsNone(form.me_office_error)
+        self.assertIsFalse("me_office" in form.errors)
 
     def test_form_validates_with_required_data(self):
         # Given a complete form
