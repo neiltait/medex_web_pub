fd7ed5bed824a01f40814b5763681f508aa95a30
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-07 14:47:44 +0000

    create pathing and basic end point to handle nav

--- cms/examinations/urls.py
+++ cms/examinations/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
   url('create', views.create_examination, name='create_examination'),
+  url(r'(?P<examination_id>[\w\-]+)', views.edit_examination, name='edit_examination'),
 ]
--- cms/examinations/views.py
+++ cms/examinations/views.py
@@ -6,19 +6,33 @@ from home.utils import redirect_to_login
 
 from users.models import User
 
+
 def create_examination(request):
-  user = User.initialise_with_token(request)
+    user = User.initialise_with_token(request)
 
-  if not user.check_logged_in():
-    return redirect_to_login()
+    if not user.check_logged_in():
+        return redirect_to_login()
 
 
-  context = {
-    'session_user': user,
-    'page_heading': 'Add a new case',
-    'sub_heading': 'Primary information',
-    'form': PrimaryExaminationInformationForm()
-  }
-  alerts = []
+    context = {
+        'session_user': user,
+        'page_heading': 'Add a new case',
+        'sub_heading': 'Primary information',
+        'form': PrimaryExaminationInformationForm()
+    }
+    alerts = []
 
-  return render(request, 'examinations/create.html', context)
+    return render(request, 'examinations/create.html', context)
+
+
+def edit_examination(request, examination_id):
+    user = User.initialise_with_token(request)
+
+    if not user.check_logged_in():
+        return redirect_to_login()
+
+    context = {
+        'session_user': user,
+    }
+
+    return render(request, 'examinations/edit.html', context)
