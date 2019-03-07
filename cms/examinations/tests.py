caf1044f07b92185b0a6d6475377ec00923ee14e
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-07 14:40:53 +0000

    start using mocks and add tests for case edit loading

--- cms/examinations/tests.py
+++ cms/examinations/tests.py
@@ -2,45 +2,46 @@ from django.conf import settings
 
 from http.cookies import SimpleCookie
 
-from requests.models import Response
-
 from rest_framework import status
 
 from unittest.mock import patch
 
-import json, uuid
+import json
 
+from medexCms.test import mocks
 from medexCms.test.utils import MedExTestCase
-
-user_obj = {
-  'user_id': '1',
-  'first_name': 'Test',
-  'last_name': 'User',
-  'email_address': 'test.user@nhs.uk',
-  'permissions': []
-}
-SUCCESSFUL_VALIDATE_SESSION = Response()
-SUCCESSFUL_VALIDATE_SESSION.status_code = status.HTTP_200_OK
-SUCCESSFUL_VALIDATE_SESSION._content = json.dumps(user_obj).encode('utf-8')
-
-UNSUCCESSFUL_VALIDATE_SESSION = Response()
-UNSUCCESSFUL_VALIDATE_SESSION.status_code = status.HTTP_401_UNAUTHORIZED
-UNSUCCESSFUL_VALIDATE_SESSION._content = json.dumps(None).encode('utf-8')
 
 
 class ExaminationsViewsTests(MedExTestCase):
 
-  #### Create case tests
-  @patch('users.request_handler.validate_session', return_value=SUCCESSFUL_VALIDATE_SESSION)
+#### Create case tests
+  @patch('users.request_handler.validate_session', return_value=mocks.SUCCESSFUL_VALIDATE_SESSION)
   def test_landing_on_create_case_page_loads_the_correct_template(self, mock_user_validation):
-    self.client.cookies = SimpleCookie({settings.AUTH_TOKEN_NAME: uuid.uuid4()})
+    self.client.cookies = SimpleCookie({settings.AUTH_TOKEN_NAME: json.dumps(mocks.AUTH_TOKEN)})
     response = self.client.get('/cases/create')
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     self.assertTemplateUsed(response, 'examinations/create.html')
 
 
-  @patch('users.request_handler.validate_session', return_value=UNSUCCESSFUL_VALIDATE_SESSION)
+  @patch('users.request_handler.validate_session', return_value=mocks.UNSUCCESSFUL_VALIDATE_SESSION)
   def test_landing_on_create_page_when_not_logged_in_redirects_to_login(self, mock_user_validation):
     response = self.client.get('/cases/create')
     self.assertEqual(response.status_code, status.HTTP_302_FOUND)
     self.assertEqual(response.url, '/login')
+
+
+#### Edit case tests
+
+  @patch('users.request_handler.validate_session', return_value=mocks.UNSUCCESSFUL_VALIDATE_SESSION)
+  def test_landing_on_edit_page_when_not_logged_in_redirects_to_login(self, mock_user_validation):
+    response = self.client.get('/cases/%s' % mocks.CREATED_EXAMINATION_ID)
+    self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+    self.assertEqual(response.url, '/login')
+
+
+  @patch('users.request_handler.validate_session', return_value=mocks.SUCCESSFUL_VALIDATE_SESSION)
+  def test_landing_on_edit_page_when_logged_in_loads_the_correct_template(self, mock_user_validation):
+    self.client.cookies = SimpleCookie({settings.AUTH_TOKEN_NAME: json.dumps(mocks.AUTH_TOKEN)})
+    response = self.client.get('/cases/%s' % mocks.CREATED_EXAMINATION_ID)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertTemplateUsed(response, 'examinations/edit.html')
