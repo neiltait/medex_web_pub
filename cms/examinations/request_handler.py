43db652e0de49c4839f5dfa9ab235ef301e41aa8
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2019-03-08 10:56:25 +0000

    move request handlers for locations to individual module

--- cms/examinations/request_handler.py
+++ cms/examinations/request_handler.py
@@ -11,14 +11,5 @@ def get_coroner_statuses_list():
 
     return [{'status': 'blocked'}]
 
-
-def get_locations_list():
-    return requests.get(settings.API_URL + '/locations/get_locations_list').json()
-
-
-def get_me_offices_list():
-    return requests.get(settings.API_URL + '/me_offices/get_me_offices_list').json()
-
-
 def post_new_examination(examination_object):
     return requests.post('%s/cases/create' % settings.API_URL, data=json.dumps(examination_object))
