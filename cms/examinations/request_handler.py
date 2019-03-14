f9f1720676bd99fcb4b4ad20161f961e2321c000
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-14 18:15:13 +0000

    add request handler for loading an examination by id

--- cms/examinations/request_handler.py
+++ cms/examinations/request_handler.py
@@ -3,13 +3,15 @@ import json
 import requests
 from django.conf import settings
 
-from examinations.forms import PrimaryExaminationInformationForm
-
-
 def get_coroner_statuses_list():
     # return requests.get(settings.API_URL + '/datatype/coroner_status')
 
     return [{'status': 'blocked'}]
 
+
 def post_new_examination(examination_object):
     return requests.post('%s/cases/create' % settings.API_URL, data=json.dumps(examination_object))
+
+
+def load_by_id(examination_id):
+    return requests.get('%s/examinations/%s' % (settings.API_URL, examination_id))
