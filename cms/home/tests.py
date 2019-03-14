a65b37753be643f2468dcf19c67f7449a0382e45
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-14 12:34:48 +0000

    fix tests for updated cookie requirments

--- cms/home/tests.py
+++ cms/home/tests.py
@@ -1,14 +1,6 @@
-from django.conf import settings
-
-from http.cookies import SimpleCookie
-
-from requests.models import Response
-
 from rest_framework import status
 
 from unittest.mock import patch
-
-import json, uuid
 
 from medexCms.test import mocks
 from medexCms.test.utils import MedExTestCase
@@ -16,121 +8,91 @@ from medexCms.test.utils import MedExTestCase
 from .utils import redirect_to_landing, redirect_to_login
 
 
-user_obj = {
-  'user_id': '1',
-  'first_name': 'Test',
-  'last_name': 'User',
-  'email_address': 'test.user@nhs.uk',
-  'permissions': []
-}
-SUCCESSFUL_VALIDATE_SESSION = Response()
-SUCCESSFUL_VALIDATE_SESSION.status_code = status.HTTP_200_OK
-SUCCESSFUL_VALIDATE_SESSION._content = json.dumps(user_obj).encode('utf-8')
-
-UNSUCCESSFUL_VALIDATE_SESSION = Response()
-UNSUCCESSFUL_VALIDATE_SESSION.status_code = status.HTTP_401_UNAUTHORIZED
-UNSUCCESSFUL_VALIDATE_SESSION._content = json.dumps(None).encode('utf-8')
-
-auth_token = 'f0c2474a-ae57-4e00-b467-cd6caaa4c466'
-
-SUCCESSFUL_SESSION_CREATION = Response()
-SUCCESSFUL_SESSION_CREATION.status_code = status.HTTP_200_OK
-SUCCESSFUL_SESSION_CREATION._content = json.dumps({'auth_token': auth_token}).encode('utf-8')
-
-UNSUCCESSFUL_SESSION_CREATION = Response()
-UNSUCCESSFUL_SESSION_CREATION.status_code = status.HTTP_401_UNAUTHORIZED
-UNSUCCESSFUL_SESSION_CREATION._content = json.dumps({'auth_token': None}).encode('utf-8')
-
 class HomeViewsTests(MedExTestCase):
 
-#### Login tests
+    # Login tests
 
-  def test_landing_on_login_page_loads_the_correct_template_with_empty_context(self):
-    response = self.client.get('/login')
-    self.assertTemplateUsed(response, 'home/login.html')
-    try: 
-      self.assertEqual(self.get_context_value(response.context, 'email_address'), None)
-      self.assertFalse('Test failed to produce expected key error')
-    except KeyError:
-      self.assertTrue('Test produced expected key error')
+    def test_landing_on_login_page_loads_the_correct_template_with_empty_context(self):
+        response = self.client.get('/login')
+        self.assertTemplateUsed(response, 'home/login.html')
+        try:
+            self.assertEqual(self.get_context_value(response.context, 'email_address'), None)
+            self.assertFalse('Test failed to produce expected key error')
+        except KeyError:
+            self.assertTrue('Test produced expected key error')
 
+    @patch('users.request_handler.validate_session', return_value=mocks.SUCCESSFUL_VALIDATE_SESSION)
+    def test_login_returns_redirect_to_landing_page_if_user_logged_in(self, mock_auth_validation):
+        self.set_auth_cookies()
+        response = self.client.get('/login')
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(response.url, '/')
 
-  @patch('users.request_handler.validate_session', return_value=SUCCESSFUL_VALIDATE_SESSION)
-  def test_login_returns_redirect_to_landing_page_if_user_logged_in(self, mock_auth_validation):
+    # Logout tests
 
-    self.client.cookies = SimpleCookie({settings.AUTH_TOKEN_NAME: uuid.uuid4()})
-    response = self.client.get('/login')
-    self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-    self.assertEqual(response.url, '/')
+    @patch('home.request_handler.end_session', return_value=mocks.SUCCESSFUL_LOGOUT)
+    def test_logout_returns_redirect_to_login_page_on_submission(self, mock_logout):
+        self.set_auth_cookies()
+        response = self.client.get('/logout')
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(response.url, '/login')
 
+    # Login callback tests
 
-#### Logout tests
+    @patch('home.request_handler.create_session', return_value=mocks.SUCCESSFUL_TOKEN_GENERATION)
+    def test_login_callback_returns_redirect_to_landing_page(self, mock_token_generation):
+        response = self.client.get('/login-callback?code=c15be3d1-513f-49dc-94f9-47449c1cfeb8')
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(response.url, '/')
 
-  @patch('home.request_handler.end_session', return_value=mocks.SUCCESSFUL_LOGOUT)
-  def test_logout_returns_redirect_to_login_page_on_submission(self, mock_logout):
-    self.client.cookies = SimpleCookie({settings.AUTH_TOKEN_NAME: json.dumps(mocks.AUTH_TOKEN)})
-    response = self.client.get('/logout')
-    self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-    self.assertEqual(response.url, '/login')
+    # Index tests
 
-#### Login callback tests
+    @patch('users.request_handler.validate_session', return_value=mocks.SUCCESSFUL_VALIDATE_SESSION)
+    def test_landing_on_the_landing_page_returns_the_correct_template(self, mock_auth_validation):
+        #TODO expand the test once the page is filled out
+        self.set_auth_cookies()
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(response, 'home/index.html')
 
-  @patch('home.request_handler.create_session', return_value=mocks.SUCCESSFUL_TOKEN_GENERATION)
-  def test_login_callback_returns_redirect_to_landing_page(self, mock_token_generation):
-    response = self.client.get('/login-callback?code=c15be3d1-513f-49dc-94f9-47449c1cfeb8')
-    self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-    self.assertEqual(response.url, '/')
+    def test_landing_on_the_landing_page_redirects_to_login_if_the_user_not_logged_in(self):
+        #TODO expand the test once the page is filled out
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(response.url, '/login')
 
-#### Index tests
-  @patch('users.request_handler.validate_session', return_value=SUCCESSFUL_VALIDATE_SESSION)
-  def test_landing_on_the_landing_page_returns_the_correct_template(self, mock_auth_validation):
-    #TODO expand the test once the page is filled out
-    self.client.cookies = SimpleCookie({settings.AUTH_TOKEN_NAME: uuid.uuid4()})
-    response = self.client.get('/')
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertTemplateUsed(response, 'home/index.html')
+    # Settings index tests
 
-  def test_landing_on_the_landing_page_redirects_to_login_if_the_user_not_logged_in(self):
-    #TODO expand the test once the page is filled out
-    response = self.client.get('/')
-    self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-    self.assertEqual(response.url, '/login')
+    @patch('users.request_handler.validate_session', return_value=mocks.SUCCESSFUL_VALIDATE_SESSION)
+    def test_landing_on_settigs_page_returns_the_correct_template_and_content_if_you_are_logged_in(self, mock_auth_validation):
+        self.set_auth_cookies()
+        response = self.client.get('/settings')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(response, 'home/settings_index.html')
 
-
-#### Settings index tests
-
-  @patch('users.request_handler.validate_session', return_value=SUCCESSFUL_VALIDATE_SESSION)
-  def test_landing_on_settigs_page_returns_the_correct_template_and_content_if_you_are_logged_in(self, mock_auth_validation):
-    self.client.cookies = SimpleCookie({settings.AUTH_TOKEN_NAME: uuid.uuid4()})
-    response = self.client.get('/settings')
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertTemplateUsed(response, 'home/settings_index.html')
-
-
-  @patch('users.request_handler.validate_session', return_value=UNSUCCESSFUL_VALIDATE_SESSION)
-  def test_landing_on_settigs_page_returns_the_correct_template_and_content_if_you_are_not_logged_in(self, mock_auth_validation):
-    response = self.client.get('/settings')
-    self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-    self.assertEqual(response.url, '/login')
+    @patch('users.request_handler.validate_session', return_value=mocks.UNSUCCESSFUL_VALIDATE_SESSION)
+    def test_landing_on_settigs_page_returns_the_correct_template_and_content_if_you_are_not_logged_in(self, mock_auth_validation):
+        response = self.client.get('/settings')
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(response.url, '/login')
 
 
 class HomeFormsTests(MedExTestCase):
-  pass
+    pass
 
 
 class HomeUtilsTests(MedExTestCase):
 
-#### Redirect to landing tests
+    # Redirect to landing tests
 
-  def test_redirect_to_landing_returns_the_correct_status_code_and_path(self):
-    result = redirect_to_landing()
-    self.assertEqual(result.status_code, status.HTTP_302_FOUND)
-    self.assertEqual(result.url, '/')
+    def test_redirect_to_landing_returns_the_correct_status_code_and_path(self):
+        result = redirect_to_landing()
+        self.assertEqual(result.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(result.url, '/')
 
+    # Redirect to login tests
 
-#### Redirect to login tests
-
-  def test_redirect_to_login_returns_the_correct_status_code_and_path(self):
-    result = redirect_to_login()
-    self.assertEqual(result.status_code, status.HTTP_302_FOUND)
-    self.assertEqual(result.url, '/login')
+    def test_redirect_to_login_returns_the_correct_status_code_and_path(self):
+        result = redirect_to_login()
+        self.assertEqual(result.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(result.url, '/login')
