98f6238fe58762d5cd6e8dbdbf255df79949796c
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-19 20:12:22 +0000

    update bereaved section to not show any on page load unless they have previously been entered

--- cms/medexCms/staticfiles/js/examinations/bereaved-info-form.js
+++ cms/medexCms/staticfiles/js/examinations/bereaved-info-form.js
@@ -7,32 +7,44 @@
 
     AdditionalBereaved.prototype = {
         setup: function () {
-          this.addBtn = this.wrapper.find('#add-another-btn');
-          this.removeBtn = this.wrapper.find('#remove-btn');
-          this.additionalShowing = 0;
-          this.additionalBereaved = this.wrapper.find('.bereaved');
-          this.startWatchers();
+            this.addBtn = this.wrapper.find('#add-another-btn');
+            this.removeBtn = this.wrapper.find('#remove-btn');
+            this.additionalShowing = 0;
+            this.additionalBereaved = this.wrapper.find('.bereaved');
+            this.startWatchers();
+            this.setInitialView();
+        },
+
+        setInitialView: function() {
+            for (var i = 0; i < this.additionalBereaved.length; i++) {
+                if($(this.additionalBereaved[i]).find('input.bereaved-name')[0].value !== '') {
+                    $(this.additionalBereaved[i]).show();
+                    this.additionalShowing++;
+                    this.removeBtn.show();
+                }
+            }
         },
 
         startWatchers: function() {
-          var that = this;
-          this.addBtn.click(function() {
-            $(that.additionalBereaved[that.additionalShowing]).show();
-            that.removeBtn.show();
-            that.additionalShowing++;
-            if (that.additionalShowing === that.additionalBereaved.length) {
-              that.addBtn.hide();
-            }
-          });
+            var that = this;
+            this.addBtn.click(function() {
+                $(that.additionalBereaved[that.additionalShowing]).show();
+                that.removeBtn.show();
+                that.additionalShowing++;
 
-          this.removeBtn.click(function() {
-            that.additionalShowing--;
-            that.addBtn.show();
-            $(that.additionalBereaved[that.additionalShowing]).hide();
-            if (that.additionalShowing === 0) {
-              that.removeBtn.hide();
-            }
-          });
+                if (that.additionalShowing === that.additionalBereaved.length) {
+                  that.addBtn.hide();
+                }
+            });
+
+            this.removeBtn.click(function() {
+                that.additionalShowing--;
+                that.addBtn.show();
+                $(that.additionalBereaved[that.additionalShowing]).hide();
+                if (that.additionalShowing === 0) {
+                    that.removeBtn.hide();
+                }
+            });
         }
     }
 
