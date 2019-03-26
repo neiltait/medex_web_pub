e46a9f7defbef88cdda0fc8c69d470a203d5f9ee
Tom Ridd <twridd@gmail.com>
2019-03-26 10:05:03 +0000

    make meo/me name appear in dropdown

--- cms/people/request_handler.py
+++ cms/people/request_handler.py
@@ -5,11 +5,24 @@ from medexCms.test import mocks
 
 
 def get_medical_examiners_list(auth_token):
-    return mocks.SUCCESSFUL_MEDICAL_EXAMINERS
-
-    # return MedexRequest.get(auth_token, "%s/MedicalExaminers" % settings.API_URL).json()
+    if settings.LOCAL:
+        return mocks.SUCCESSFUL_MEDICAL_EXAMINERS
+    else:
+        response_users = MedexRequest.get(auth_token, "%s/users/medical_examiners" % settings.API_URL).json()['users']
+        return [convert_user(response_user) for response_user in response_users]
 
 
 def get_medical_examiners_officers_list(auth_token):
-    return mocks.SUCCESSFUL_MEDICAL_EXAMINERS_OFFICERS
-    # return MedexRequest.get(auth_token, "%s/MedicalExaminerOfficers" % settings.API_URL).json()
+    if settings.LOCAL:
+        return mocks.SUCCESSFUL_MEDICAL_EXAMINERS_OFFICERS
+    else:
+        response_users = MedexRequest.get(auth_token, "%s/users/medical_examiner_officers" % settings.API_URL).json()['users']
+        return [convert_user(response_user) for response_user in response_users]
+
+
+def convert_user(response_user):
+    return {
+        "first_name": response_user['firstName'],
+        "last_name": response_user['lastName'],
+        "user_id": response_user['userId']
+    }
