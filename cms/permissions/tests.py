c29d5e12da5f1806126ae7e4cb8a9aa7166e68bb
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-26 12:06:25 +0000

    add in missing permission model tests

--- cms/permissions/tests.py
+++ cms/permissions/tests.py
@@ -1,6 +1,39 @@
 from alerts import messages
+from medexCms.test import mocks
 from medexCms.test.utils import MedExTestCase
+
 from permissions.forms import PermissionBuilderForm
+from permissions.models import Permission
+
+
+class PermissionModelsTests(MedExTestCase):
+
+    #### Permission tests
+    def test_correctly_sets_values_on_init(self):
+        permission = Permission(mocks.PERMISSION_OBJECT)
+        self.assertEqual(permission.user_id, mocks.PERMISSION_OBJECT['userId'])
+        self.assertEqual(permission.permission_id, mocks.PERMISSION_OBJECT['permissionId'])
+        self.assertEqual(permission.location_id, mocks.PERMISSION_OBJECT['locationId'])
+        self.assertEqual(permission.user_role, mocks.PERMISSION_OBJECT['userRole'])
+
+    def test_role_type_returns_the_correct_value_from_the_enum(self):
+        permission = Permission(mocks.PERMISSION_OBJECT)
+
+        role_key = '0'
+        permission.user_role = role_key
+        self.assertEqual(permission.role_type(), permission.ROLES[role_key])
+
+        role_key = '1'
+        permission.user_role = role_key
+        self.assertEqual(permission.role_type(), permission.ROLES[role_key])
+
+        role_key = '2'
+        permission.user_role = role_key
+        self.assertEqual(permission.role_type(), permission.ROLES[role_key])
+
+        role_key = '3'
+        permission.user_role = role_key
+        self.assertEqual(permission.role_type(), permission.ROLES[role_key])
 
 
 class UsersFormsTests(MedExTestCase):
