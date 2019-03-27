53bc55d36c61ce6b9d23f2da93c682e6653bafce
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-27 22:15:37 +0000

    load permitted people in to the dropdown model

--- cms/locations/models.py
+++ cms/locations/models.py
@@ -1,3 +1,4 @@
+from people.models import DropdownPerson
 from . import request_handler
 
 
@@ -21,5 +22,8 @@ class Location:
         return self
 
     def load_permitted_users(self, auth_token):
-        users = request_handler.get_permitted_users(auth_token, self.location_id)
+        users = []
+        users_data = request_handler.get_permitted_users(auth_token, self.location_id)['users']
+        for user in users_data:
+            users.append(DropdownPerson(user))
         return users
