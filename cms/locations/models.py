1e86d97209ac81ad386d7edc055cd715398b1352
Matt Nicks <matt.nicks@methods.co.uk>
2019-03-27 13:26:58 +0000

    create initial Location model class

--- cms/locations/models.py
+++ cms/locations/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from . import request_handler
 
-# Create your models here.
+
+class Location:
+
+    def __init__(self):
+        self.location_id = ''
+        self.name = ''
+        self.parent_id = ''
+
+    @classmethod
+    def initialise_with_id(cls, location_id):
+        location = Location()
+        location.location_id = location_id
+        return location
+
+    def load_permitted_users(self, auth_token):
+        users = request_handler.get_permitted_users(auth_token, self.location_id)
+        return users
