83a099be36f8539d75386c947237565e77ef91c7
Matt Nicks <matt.nicks@methods.co.uk>
2019-04-10 16:15:41 +0100

    extend form parser and submitter to handle other event forms

--- cms/examinations/utils.py
+++ cms/examinations/utils.py
@@ -1,18 +1,22 @@
-from examinations.forms import PreScrutinyEventForm
+from examinations.forms import PreScrutinyEventForm, OtherEventForm
 from . import request_handler
 
 PRE_SCRUTINY_FORM = 'pre-scrutiny'
+OTHER_FORM = 'other'
 
 
 def event_form_parser(form_data):
     event_type = form_data.get('add-event-to-timeline') if form_data.get('add-event-to-timeline') else \
         form_data.get('save-as-draft')
-    print(event_type)
     if event_type == PRE_SCRUTINY_FORM:
         return PreScrutinyEventForm(form_data)
+    elif event_type == OTHER_FORM:
+        return OtherEventForm(form_data)
 
 
 def event_form_submitter(auth_token, examination_id, form):
     form_type = type(form)
     if form_type == PreScrutinyEventForm:
         return request_handler.create_pre_scrutiny_event(auth_token, examination_id, form.for_request())
+    elif form_type == OtherEventForm:
+        return request_handler.create_other_event(auth_token, examination_id, form.for_request())
