4d744a648e786aa635a4627b0ec09ba199d39402
Matt Nicks <matt.nicks@methods.co.uk>
2019-04-10 17:44:55 +0100

    extend form parser and submitter to handle meo summary event forms

--- cms/examinations/utils.py
+++ cms/examinations/utils.py
@@ -1,7 +1,8 @@
-from examinations.forms import PreScrutinyEventForm
+from examinations.forms import PreScrutinyEventForm, MeoSummaryEventForm
 from . import request_handler
 
 PRE_SCRUTINY_FORM = 'pre-scrutiny'
+MEO_SUMMARY_FORM = 'meo-summary'
 
 
 def event_form_parser(form_data):
@@ -10,9 +11,13 @@ def event_form_parser(form_data):
     print(event_type)
     if event_type == PRE_SCRUTINY_FORM:
         return PreScrutinyEventForm(form_data)
+    elif event_type == MEO_SUMMARY_FORM:
+        return MeoSummaryEventForm(form_data)
 
 
 def event_form_submitter(auth_token, examination_id, form):
     form_type = type(form)
     if form_type == PreScrutinyEventForm:
         return request_handler.create_pre_scrutiny_event(auth_token, examination_id, form.for_request())
+    elif form_type == MeoSummaryEventForm:
+        return request_handler.create_meo_summary_event(auth_token, examination_id, form.for_request())
