391111959308d3c0fcd63fdd7b8f3b8013606883
Matt Nicks <matt.nicks@methods.co.uk>
2019-04-15 15:32:56 +0100

    create patient details header class

--- cms/examinations/models.py
+++ cms/examinations/models.py
@@ -851,3 +851,32 @@ class CaseOutcome:
     def display_representative_outcome(self):
         return self.REPRESENTATIVE_OUTCOMES.get(self.case_representative_outcome)
 
+
+class PatientHeader:
+    date_format = '%d.%m.%Y'
+
+    def __init__(self, obj_dict):
+        self.urgency_score = obj_dict.get("urgencyScore")
+        self.given_names = obj_dict.get("givenNames")
+        self.surname = obj_dict.get("surname")
+        self.nhs_number = obj_dict.get("nhsNumber")
+        self.id = obj_dict.get("examinationId")
+        self.time_of_death = obj_dict.get("timeOfDeath")
+        self.date_of_birth = parse_datetime(obj_dict.get("dateOfBirth"))
+        self.date_of_death = parse_datetime(obj_dict.get("dateOfDeath"))
+        self.appointment_date = parse_datetime(obj_dict.get("appointmentDate"))
+        self.appointment_time = obj_dict.get("appointmentTime")
+        self.last_admission = parse_datetime(obj_dict.get("lastAdmission"))
+        self.case_created_date = parse_datetime(obj_dict.get("caseCreatedDate"))
+        self.admission_notes_added = obj_dict.get("admissionNotesHaveBeenAdded")
+        self.ready_for_me_scrutiny = obj_dict.get("readyForMEScrutiny")
+        self.unassigned = obj_dict.get("unassigned")
+        self.have_been_scrutinised = obj_dict.get("haveBeenScrutinisedByME")
+        self.pending_admission_notes = obj_dict.get("pendingAdmissionNotes")
+        self.pending_discussion_with_qap = obj_dict.get("pendingDiscussionWithQAP")
+        self.pending_discussion_with_representative = obj_dict.get("pendingDiscussionWithRepresentative")
+        self.have_final_case_outstanding_outcomes = obj_dict.get("haveFinalCaseOutstandingOutcomes")
+
+    @property
+    def full_name(self):
+        return "%s %s" % (self.given_names, self.surname)
