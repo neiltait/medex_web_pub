03ff50400cc07e8931e84dd5a5f4ad51b2d8cf2c
Matt Nicks <matt.nicks@methods.co.uk>
2019-04-16 12:55:50 +0100

    refactor change tab js in to separate file

--- cms/medexCms/staticfiles/js/examinations/examination-edit.js
+++ cms/medexCms/staticfiles/js/examinations/examination-edit.js
@@ -110,41 +110,6 @@
     }
   }
 
-  var ChangeTabModal = function (modal, saveCallBack) {
-    this.modal = $(modal);
-    this.saveCallBack = saveCallBack;
-    this.setup();
-  }
-
-  ChangeTabModal.prototype = {
-    setup: function () {
-      this.saveButton = this.modal.find('#save-continue');
-      this.discardButton = this.modal.find('#discard');
-      this.startWatchers();
-    },
-
-    startWatchers: function () {
-      var that = this;
-
-      this.saveButton.click(function () {
-        that.saveCallBack(that.nextTab);
-      });
-
-      this.discardButton.click(function () {
-        var pathPieces = location.pathname.split('/');
-        pathPieces[pathPieces.length - 1] = that.nextTab;
-        newPath = pathPieces.join('/');
-        location.pathname = newPath;
-      });
-    },
-
-    show: function (nextTab) {
-      this.nextTab = nextTab;
-      this.modal.show();
-    }
-  }
-
-
   var AdditionalNotesSection = function (section) {
     this.section = $(section);
     this.setup();
