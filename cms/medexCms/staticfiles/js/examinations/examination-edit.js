611bf884b3bdac3883bfeed1fb314f28073bd331
Matt Nicks <matt.nicks@methods.co.uk>
2019-04-16 14:02:26 +0100

    refactor additional notes section in to separate file

--- cms/medexCms/staticfiles/js/examinations/examination-edit.js
+++ cms/medexCms/staticfiles/js/examinations/examination-edit.js
@@ -90,39 +90,6 @@
     }
   }
 
-  var AdditionalNotesSection = function (section) {
-    this.section = $(section);
-    this.setup();
-  };
-
-  AdditionalNotesSection.prototype = {
-    setup: function () {
-      this.button = this.section.find('.additional-notes-button');
-      this.panel = this.section.find('.additional-notes-panel');
-      this.notes = this.section.find('textarea');
-
-      this.setupWatchers();
-
-      this.openPopulatedPanelOnStartup()
-    },
-    setupWatchers: function() {
-      let that = this;
-      this.button.on('click', function (event) {
-        event.preventDefault()
-        that.openPanel()
-      })
-    },
-    openPanel: function () {
-      this.panel.removeClass("medex-hidden")
-      this.button.addClass("medex-hidden")
-    },
-    openPopulatedPanelOnStartup: function () {
-      if(this.notes.val() !== '') {
-        this.openPanel()
-      }
-    }
-  };
-
   var AddRemovePanelList = function (section, visibleCount) {
     this.visibleCount = visibleCount;
     this.panels = section.find('.add-remove-panel');
@@ -177,7 +144,6 @@
       }
     }
   };
-
 
   var AddRemoveItemSection = function (section) {
     this.visible = false;
---
+++ cms/medexCms/staticfiles/js/examinations/partials/additional-notes-section.js
@@ -0,0 +1,35 @@
+var AdditionalNotesSection = function (section) {
+    this.section = $(section);
+    this.setup();
+};
+
+AdditionalNotesSection.prototype = {
+    setup: function () {
+      this.button = this.section.find('.additional-notes-button');
+      this.panel = this.section.find('.additional-notes-panel');
+      this.notes = this.section.find('textarea');
+
+      this.setupWatchers();
+
+      this.openPopulatedPanelOnStartup()
+    },
+
+    setupWatchers: function() {
+        let that = this;
+        this.button.on('click', function (event) {
+            event.preventDefault()
+            that.openPanel()
+        });
+    },
+
+    openPanel: function () {
+        this.panel.removeClass("medex-hidden")
+        this.button.addClass("medex-hidden")
+    },
+
+    openPopulatedPanelOnStartup: function () {
+        if(this.notes.val() !== '') {
+            this.openPanel()
+        }
+    }
+};
