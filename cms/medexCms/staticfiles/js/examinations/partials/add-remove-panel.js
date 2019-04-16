aeebfb9e094fe7dcd60ef3965b4b62366dfda343
Matt Nicks <matt.nicks@methods.co.uk>
2019-04-16 14:16:19 +0100

    refactor add remove panel into separate file

---
+++ cms/medexCms/staticfiles/js/examinations/partials/add-remove-panel.js
@@ -0,0 +1,59 @@
+var AddRemovePanelList = function (section, visibleCount) {
+    this.visibleCount = visibleCount;
+    this.panels = section.find('.add-remove-panel');
+    this.addButton = section.find('.add-panel-button');
+    this.removeButton = section.find('.remove-panel-button');
+    this.setup();
+};
+
+AddRemovePanelList.prototype = {
+    setup: function () {
+        let that = this;
+        this.addButton.on('click', function (event) {
+            event.preventDefault();
+            if (that.visibleCount < that.panels.length) {
+                that.visibleCount += 1;
+                that.refreshVisible();
+            }
+        });
+
+        this.removeButton.on('click', function (event) {
+            event.preventDefault();
+            if (that.visibleCount > 0) {
+                that.visibleCount -= 1;
+                that.refreshVisible();
+            }
+        });
+
+        this.refreshVisible();
+    },
+
+    refreshVisible() {
+        this.refreshVisiblePanels();
+        this.refreshVisibleButtons();
+    },
+
+    refreshVisiblePanels() {
+        for (i = 0; i < this.panels.length; i += 1) {
+            if (i < this.visibleCount) {
+                this.panels[i].classList.remove("medex-hidden");
+            } else {
+                this.panels[i].classList.add("medex-hidden");
+            }
+        }
+    },
+
+    refreshVisibleButtons() {
+        if (this.visibleCount === 0) {
+            this.removeButton.addClass("medex-hidden");
+        } else {
+            this.removeButton.removeClass("medex-hidden");
+        }
+
+        if (this.visibleCount === this.panels.length) {
+            this.addButton.addClass("medex-hidden");
+        } else {
+            this.addButton.removeClass("medex-hidden");
+        }
+    }
+};
