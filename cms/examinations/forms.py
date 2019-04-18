f58a1f7f82ea928e5fab7ac8850166ef0265be45
Matt Nicks <matt.nicks@methods.co.uk>
2019-04-18 17:50:50 +0100

    add for request function to form to prep for API  submission

--- cms/examinations/forms.py
+++ cms/examinations/forms.py
@@ -814,3 +814,10 @@ class OutstandingItemsForm:
         self.mccd_issued = form_data.get('mccd_issued')
         self.cremation_form = form_data.get('cremation_form')
         self.gp_notified = form_data.get('gp_notified')
+
+    def for_request(self):
+        return {
+            "mccdIssed": True if self.mccd_issued == 'true' else False,
+            "cremationFormStatus": self.cremation_form,
+            "gpNotifedStatus": self.gp_notified
+        }
