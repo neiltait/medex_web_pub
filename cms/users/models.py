6ddc39737594ead8a2b4ccb987b8c58a8efbef8b
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-01 16:10:04 +0100

    only show the amend link if the user could have created that type of event in the first place

--- cms/users/models.py
+++ cms/users/models.py
@@ -214,3 +214,22 @@ class User:
             ]
         else:
             logger.error('Unknown role type')
+
+    def editable_event_types(self):
+        if self.role_type == self.MEO_ROLE_TYPE:
+            return [
+                'admission-notes',
+                'medical-history',
+                'meo-summary',
+                'other',
+            ]
+        elif self.role_type == self.ME_ROLE_TYPE:
+            return [
+                'pre-scrutiny',
+                'qap-discussion',
+                'bereaved-discussion',
+                'other',
+            ]
+        else:
+            logger.error('Unknown role type')
+            return []
