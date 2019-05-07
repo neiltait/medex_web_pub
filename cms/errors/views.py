1ed643c1a7aa45d857601135b9f0a9264db2ca85
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-07 14:13:33 +0100

    add in error view for users going to urls they are not permitted to

--- cms/errors/views.py
+++ cms/errors/views.py
@@ -1,4 +1,4 @@
-from errors.models import MethodNotAllowedError
+from errors.models import MethodNotAllowedError, NotPermittedError
 
 
 def __handle_method_not_allowed_error(user):
@@ -11,3 +11,15 @@ def __handle_method_not_allowed_error(user):
     }
 
     return template, context, result.status_code
+
+
+def __handle_not_permitted_error(user):
+    template = 'errors/base_error.html'
+    result = NotPermittedError()
+
+    context = {
+        'session_user': user,
+        'error': result
+    }
+
+    return template, context, result.http_code
