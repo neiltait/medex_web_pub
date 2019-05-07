0a9860bc4e266b0d819551916afa61dfb231edf2
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-07 11:33:20 +0100

    add gunicorn runner

---
+++ cms/deploy_files/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+#Run Gunicorn
+exec gunicorn medexCms.wsgi:application \
+  --name medex-cms \
+  --bind 0.0.0.0:8000 \
+  --workers 3 \
+  --log-level=info \
+  --log-file=- \
+  --access-logfile=- \
+  --error-logfile=- \
+  --timeout 60
+
+
+# EXECUTE DOCKER COMMAND NOW
+exec "$@"
