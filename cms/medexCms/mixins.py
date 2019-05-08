341838d4e902730e88c6481e7d622b0030db17af
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-08 14:28:39 +0100

    create permission_required mixin

--- cms/medexCms/mixins.py
+++ cms/medexCms/mixins.py
@@ -1,5 +1,7 @@
+from django.core.exceptions import ImproperlyConfigured
+
 from errors.utils import log_unexpected_method
-from errors.views import handle_method_not_allowed_error
+from errors.views import handle_method_not_allowed_error, handle_not_permitted_error
 from home.utils import redirect_to_login
 from users.models import User
 
@@ -15,3 +17,42 @@ class LoginRequiredMixin:
     def http_method_not_allowed(self, request, *args, **kwargs):
         log_unexpected_method(request.method, request.path)
         return handle_method_not_allowed_error(request, self.user)
+
+
+class PermissionRequiredMixin:
+    """Verify that the current user has all specified permissions."""
+    permission_required = None
+
+    def get_permission_required(self):
+        """
+        Override this method to override the permission_required attribute.
+        Must return an iterable.
+        """
+        if self.permission_required is None:
+            raise ImproperlyConfigured(
+                '{0} is missing the permission_required attribute. Define {0}.permission_required, or override '
+                '{0}.get_permission_required().'.format(self.__class__.__name__)
+            )
+        if isinstance(self.permission_required, str):
+            perms = (self.permission_required,)
+        else:
+            perms = self.permission_required
+        return perms
+
+    def has_permission(self):
+        """
+        Override this method to customize the way permissions are checked.
+        """
+        perms = self.get_permission_required()
+        for perm in perms:
+            if not getattr(self.user.permitted_actions, perm, False):
+                return False
+        return True
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.has_permission():
+            return self.handle_no_permission(request)
+        return super().dispatch(request, *args, **kwargs)
+
+    def handle_no_permission(self, request):
+        return handle_not_permitted_error(request, self.user)
