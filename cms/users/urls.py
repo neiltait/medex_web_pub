17e934fe63d936bc895946de380b5669728e0ed7
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-08 12:41:13 +0100

    update urls to use new view class

--- cms/users/urls.py
+++ cms/users/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
+from users.views import CreateUserView
 from . import views
 
 urlpatterns = [
-  url(r'new', views.create_user, name='create_user'),
+  url(r'new', CreateUserView.as_view(), name='create_user'),
   url(r'(?P<user_id>[\w\-]+)/add_permission', views.add_permission, name='add_permission')
 ]
