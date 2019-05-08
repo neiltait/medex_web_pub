345b9a7b92d184f637a24773d91e3bf7df3b3112
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-08 12:38:59 +0100

    create temporary alternative error method compatible with class based views

--- cms/errors/views.py
+++ cms/errors/views.py
@@ -1,4 +1,18 @@
-from errors.models import MethodNotAllowedError
+from django.shortcuts import render
+
+from .models import MethodNotAllowedError
+
+
+def handle_method_not_allowed_error(request, user):
+    template = 'errors/base_error.html'
+    result = MethodNotAllowedError()
+
+    context = {
+        'session_user': user,
+        'error': result,
+    }
+
+    return render(request, template, context, status=result.status_code)
 
 
 def __handle_method_not_allowed_error(user):
@@ -11,3 +25,4 @@ def __handle_method_not_allowed_error(user):
     }
 
     return template, context, result.status_code
+
