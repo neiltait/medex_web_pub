1570592ab4e5e4d63d323170f44e21800db4b184
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-08 12:42:18 +0100

    refcator add permission view in to a class in the right file

--- cms/permissions/views.py
+++ cms/permissions/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework import status
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from errors.utils import log_api_error
+from medexCms.mixins import LoginRequiredMixin
+from medexCms.views import MedExBaseView
+from permissions.forms import PermissionBuilderForm
+from users.views import ManageUserBaseView
+
+
+class AddPermissionView(LoginRequiredMixin, ManageUserBaseView):
+    template = 'users/permission_builder.html'
+
+    def get(self, request):
+        status_code = status.HTTP_200_OK
+
+        context = self.__set_add_permission_context(PermissionBuilderForm(), False)
+        return render(request, self.template, context, status=status_code)
+
+    def post(self, request):
+        post_body = request.POST
+        form = PermissionBuilderForm(post_body)
+        add_another = True if post_body.get('add_another') == "true" else False
+
+        if form.is_valid():
+            response = self.managed_user.add_permission(form, self.user.auth_token)
+
+            if response.ok:
+                if add_another:
+                    return redirect('add_permission', user_id=self.managed_user.user_id)
+                else:
+                    return redirect('/settings')
+            else:
+                log_api_error('permission creation', response.text)
+                status_code = response.status_code
+
+        else:
+            status_code = status.HTTP_400_BAD_REQUEST
+
+        context = self.__set_add_permission_context(form, True)
+
+        return render(request, self.template, context, status=status_code)
+
+    def __set_add_permission_context(self, form, invalid):
+        trusts = self.user.get_permitted_trusts()
+        regions = self.user.get_permitted_regions()
+
+        return {
+            'session_user': self.user,
+            'sub_heading': 'Add role and permission level',
+            'form': form,
+            'submit_path': 'add_permission',
+            'invalid': invalid,
+            'trusts': trusts,
+            'regions': regions,
+            'managed_user': self.managed_user,
+        }
--- cms/users/views.py
+++ cms/users/views.py
@@ -55,76 +55,76 @@ class CreateUserView(LoginRequiredMixin, View):
         }
 
 
-def add_permission(request, user_id):
-    user = User.initialise_with_token(request)
-    if not user.check_logged_in():
-        return redirect_to_login()
-
-    managed_user = User.load_by_id(user_id, user.auth_token)
-    if managed_user is None:
-        return render_404(request, user, 'user')
-
-    if request.method == 'GET':
-        template, context, status_code = __get_add_permission(user, managed_user)
-
-    elif request.method == 'POST':
-        template, context, status_code, redirect_response = __post_add_permission(user, managed_user, request.POST)
-
-        if redirect_response:
-            return redirect_response
-
-    else:
-        log_unexpected_method(request.method, 'add permission')
-        template, context, status_code = __handle_method_not_allowed_error(user)
-
-    return render(request, template, context, status=status_code)
-
-
-def __get_add_permission(user, managed_user):
-    template = 'users/permission_builder.html'
-    status_code = status.HTTP_200_OK
-
-    context = __set_add_permission_context(user, PermissionBuilderForm(), False, managed_user)
-    return template, context, status_code
+# def add_permission(request, user_id):
+#     user = User.initialise_with_token(request)
+#     if not user.check_logged_in():
+#         return redirect_to_login()
+#
+#     managed_user = User.load_by_id(user_id, user.auth_token)
+#     if managed_user is None:
+#         return render_404(request, user, 'user')
+#
+#     if request.method == 'GET':
+#         template, context, status_code = __get_add_permission(user, managed_user)
+#
+#     elif request.method == 'POST':
+#         template, context, status_code, redirect_response = __post_add_permission(user, managed_user, request.POST)
+#
+#         if redirect_response:
+#             return redirect_response
+#
+#     else:
+#         log_unexpected_method(request.method, 'add permission')
+#         template, context, status_code = __handle_method_not_allowed_error(user)
+#
+#     return render(request, template, context, status=status_code)
 
 
-def __post_add_permission(user, managed_user, post_body):
-    template = 'users/permission_builder.html'
-
-    form = PermissionBuilderForm(post_body)
-    add_another = True if post_body.get('add_another') == "true" else False
-
-    if form.is_valid():
-        response = managed_user.add_permission(form, user.auth_token)
-
-        if response.ok:
-            if add_another:
-                return None, None, None, redirect('add_permission', user_id=managed_user.user_id)
-            else:
-                return None, None, None, redirect('/settings')
-        else:
-            log_api_error('permission creation', response.text)
-            status_code = response.status_code
-
-    else:
-        status_code = status.HTTP_400_BAD_REQUEST
-
-    context = __set_add_permission_context(user, form, True, managed_user)
-
-    return template, context, status_code, None
+# def __get_add_permission(user, managed_user):
+#     template = 'users/permission_builder.html'
+#     status_code = status.HTTP_200_OK
+#
+#     context = __set_add_permission_context(user, PermissionBuilderForm(), False, managed_user)
+#     return template, context, status_code
 
 
-def __set_add_permission_context(user, form, invalid, managed_user):
-    trusts = user.get_permitted_trusts()
-    regions = user.get_permitted_regions()
+# def __post_add_permission(user, managed_user, post_body):
+#     template = 'users/permission_builder.html'
+#
+#     form = PermissionBuilderForm(post_body)
+#     add_another = True if post_body.get('add_another') == "true" else False
+#
+#     if form.is_valid():
+#         response = managed_user.add_permission(form, user.auth_token)
+#
+#         if response.ok:
+#             if add_another:
+#                 return None, None, None, redirect('add_permission', user_id=managed_user.user_id)
+#             else:
+#                 return None, None, None, redirect('/settings')
+#         else:
+#             log_api_error('permission creation', response.text)
+#             status_code = response.status_code
+#
+#     else:
+#         status_code = status.HTTP_400_BAD_REQUEST
+#
+#     context = __set_add_permission_context(user, form, True, managed_user)
+#
+#     return template, context, status_code, None
 
-    return {
-        'session_user': user,
-        'sub_heading': 'Add role and permission level',
-        'form': form,
-        'submit_path': 'add_permission',
-        'invalid': invalid,
-        'trusts': trusts,
-        'regions': regions,
-        'managed_user': managed_user,
-    }
+
+# def __set_add_permission_context(user, form, invalid, managed_user):
+#     trusts = user.get_permitted_trusts()
+#     regions = user.get_permitted_regions()
+#
+#     return {
+#         'session_user': user,
+#         'sub_heading': 'Add role and permission level',
+#         'form': form,
+#         'submit_path': 'add_permission',
+#         'invalid': invalid,
+#         'trusts': trusts,
+#         'regions': regions,
+#         'managed_user': managed_user,
+#     }
