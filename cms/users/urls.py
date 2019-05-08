b995a9cf2eb49f2805b29ee4cbb15f98a413f358
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-08 14:31:52 +0100

    update the urls to use the add permission view class

--- cms/users/urls.py
+++ cms/users/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
+from permissions.views import AddPermissionView
 from users.views import CreateUserView
-from . import views
 
 urlpatterns = [
   url(r'new', CreateUserView.as_view(), name='create_user'),
-  url(r'(?P<user_id>[\w\-]+)/add_permission', views.add_permission, name='add_permission')
+  url(r'(?P<user_id>[\w\-]+)/add_permission', AddPermissionView.as_view(), name='add_permission')
 ]
