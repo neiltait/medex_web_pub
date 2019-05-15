4ee62032765873284ced709ff93d99ce4bd53024
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-15 17:41:17 +0100

    update index overview class to set the possible filter options

--- cms/home/models.py
+++ cms/home/models.py
@@ -1,3 +1,7 @@
+from locations.models import Location
+from people.models import DropdownPerson
+
+
 class IndexOverview:
 
     def __init__(self, location, response):
@@ -13,12 +17,27 @@ class IndexOverview:
         self.count_qap_discussion_pending = response.get('countOfCasesPendingDiscussionWithQAP')
         self.count_representative_discussion_pending = response.get('countOfCasesPendingDiscussionWithRepresentative')
         self.count_final_outcome_outstanding = response.get('countOfCasesHaveFinalCaseOutstandingOutcomes')
+        self.filter_locations = self.process_filter_locations(response.get('lookups').get('LocationFilterLookup'))
+        self.filter_people = self.process_filter_people(response.get('lookups').get('UserFilterLookup'))
+        self.set_location_display_name()
 
-    def set_location_display_name(self, locations):
+    def set_location_display_name(self):
         if self.location_id:
-            for location in locations:
+            for location in self.filter_locations:
                 if location.location_id == self.location_id:
                     self.location_name = location.name
         else:
             self.location_name = 'All permitted'
         return self
+
+    def process_filter_locations(self, locations_data):
+        locations = []
+        for location in locations_data:
+            locations.append(Location().set_values(location))
+        return locations
+
+    def process_filter_people(self, people_data):
+        people = []
+        for person in people_data:
+            people.append(DropdownPerson(person))
+        return people
