9c22c055c72c9b53a6d71f2cc5916786abb9e712
Matt Nicks <matt.nicks@methods.co.uk>
2019-05-20 12:46:15 +0100

    remove redundant code

--- cms/examinations/request_handler.py
+++ cms/examinations/request_handler.py
@@ -17,13 +17,6 @@ def post_new_examination(examination_object, auth_token):
         return MedexRequest.post(auth_token, '%s/examinations' % settings.API_URL, json.dumps(examination_object))
 
 
-def load_by_id(examination_id, auth_token):
-    if settings.LOCAL:
-        return ExaminationMocks.get_successful_case_load_response()
-    else:
-        return MedexRequest.get(auth_token, '%s/examinations/%s' % (settings.API_URL, examination_id))
-
-
 def load_examinations_index(params, auth_token):
     if settings.LOCAL:
         return ExaminationMocks.get_successful_case_index_response()
