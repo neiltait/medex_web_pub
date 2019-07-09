fb3be3c9ceb755b702d27e73adc4a9e53fa3999e
Tom Ridd <twridd@gmail.com>
2019-07-09 12:37:19 +0100

    update keep alive as repeated background task

--- cms/medexCms/staticfiles/js/home/keep-alive.js
+++ cms/medexCms/staticfiles/js/home/keep-alive.js
@@ -1,52 +1,40 @@
-(function ($) {
-
-    var KeepAlive = function (input) {
-        this.input = $(input);
-        this.setup();
-    };
-
-    KeepAlive.prototype = {
-        setup: function () {
-            this.startWatcher();
-        },
-
-        startWatcher: function () {
-            this.input.click(this.keepAlive.bind(this));
-        },
-
-        keepAlive: function () {
-            var refreshCookie = this.readCookie("medex_do_not_refresh");
-
-            if(!refreshCookie) {
-                this.refreshTokens();
-            }
-        },
-
-        refreshTokens: function () {
-            $.ajax({
-                url: "login-refresh",
-                dataType: "json",
-                method: "POST",
-                context: document.body
-            });
-        },
-
-        readCookie: function (name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-            }
-            return null;
-        },
-    };
-
-    function init() {
-        new KeepAlive("#test-button")
+function keepAlive() {
+    var refreshCookie = this.readCookie("medex_do_not_refresh");
+    console.log("keeping alive");
+    if (!refreshCookie) {
+        console.log("refreshing");
+        this.refreshTokens();
     }
+}
 
-    $(document).on('page:load', init);
-    $(init)
-}(jQuery));
+function refreshTokens() {
+    $.ajax({
+        url: "login-refresh",
+        dataType: "json",
+        method: "POST",
+        context: document.body
+    });
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function runKeepAliveOnThisPage() {
+    return $('#do-not-refresh-tokens').length == 0
+}
+function startKeepAlive() {
+    if(runKeepAliveOnThisPage()) {
+        keepAlive();
+        setInterval(keepAlive, 5*60*1000);
+    }
+}
+
+startKeepAlive();
