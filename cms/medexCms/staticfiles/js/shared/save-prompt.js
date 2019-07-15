80b668ae7a2296710afe6c4ce94aa02bdcca636b
Tom Ridd <twridd@gmail.com>
2019-07-15 17:11:41 +0100

    add save prompt code when multiple forms are on one page

--- cms/medexCms/staticfiles/js/shared/save-prompt.js
+++ cms/medexCms/staticfiles/js/shared/save-prompt.js
@@ -35,3 +35,48 @@ SavePromptForm.prototype = {
 
     }
 };
+
+var SavePromptWithMultipleForms = function (forms) {
+    this.forms = forms;
+    this.submittedAsForm = false;
+    this.content = [];
+    this.prompt = "Are you sure you want to leave this page?";
+    this.setup();
+};
+
+SavePromptWithMultipleForms.prototype = {
+    setup: function () {
+
+        for (var i = 0; i < this.forms.length; i++) {
+            this.content.push(this.serializeIncludingDisabled(this.forms[i]))
+            this.forms[i].submit(function (e) {
+                this.submittedAsForm = true;
+            });
+        }
+        this.bindWindow();
+    },
+    bindWindow: function () {
+        var that = this;
+        window.onbeforeunload = function (e) {
+            if (!that.submittedAsForm) {
+                for (var i = 0; i < that.forms.length; i++) {
+                    if (that.content[i] !== that.serializeIncludingDisabled(that.forms[i])) {
+                        e.returnValue = that.prompt;
+                        return that.prompt
+                    }
+                }
+            }
+        }
+    },
+
+    serializeIncludingDisabled: function (form) {
+        if(form) {
+            var disabled = form.find(':input:disabled').removeAttr('disabled');
+            var serialized = form.serialize();
+            disabled.attr('disabled', 'disabled');
+            return serialized
+        } else {
+            return ""
+        }
+    }
+};
