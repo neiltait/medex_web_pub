c6e05543c454f7de254e8526455bb999fb95c308
Tom Ridd <twridd@gmail.com>
2019-07-15 21:24:42 +0100

    handle timeouts using cookie with expiry time checked once per minute

--- cms/medexCms/staticfiles/js/home/idle-logout.js
+++ cms/medexCms/staticfiles/js/home/idle-logout.js
@@ -1,31 +1,71 @@
 var idleMinutes = 0;
 var maxIdleMinutes = 60;
+var userIsIdle = true;
 
-function timerIncrement() {
-    idleMinutes = idleMinutes + 1;
-    if (idleMinutes >= maxIdleMinutes) { // 60 minutes
+function startIdleTimer() {
+    resetCookie()
+
+    //Increment the idle time counter every minute.
+    setInterval(update, 60000); // 1 minutes
+
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        userIsIdle = false;
+    });
+    $(this).keypress(function (e) {
+        userIsIdle = false;
+    });
+}
+
+function update() {
+    if (userIsIdle) {
+        logoutIfMaxTimeExceeded()
+    } else {
+        resetCookie()
+    }
+}
+
+function logoutIfMaxTimeExceeded() {
+    if (cookieHasTimedOut()) {
         window.location = '/logout';
     }
 }
 
-function startWatchers() {
-    //Increment the idle time counter every minute.
-    setInterval(timerIncrement, 60000); // 1 minutes
+function resetCookie() {
+    createCookie("medex_logged_in", "logged in", maxIdleMinutes)
+}
 
-    //Zero the idle timer on mouse movement.
-    $(this).mousemove(function (e) {
-        idleMinutes = 0;
-    });
-    $(this).keypress(function (e) {
-        idleMinutes = 0;
-    });
+function cookieHasTimedOut() {
+    return readCookie("medex_logged_in") === null
+}
+
+function createCookie(name, value, minutes) {
+    if (minutes) {
+        var date = new Date();
+        date.setTime(date.getTime() + (minutes * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
 
 function getVariables() {
     var maxSecondsHiddenInput = $('#max-idle-seconds');
-    if(maxSecondsHiddenInput.length > 0) {
+    if (maxSecondsHiddenInput.length > 0) {
         var maxSeconds = parseInt(maxSecondsHiddenInput[0].value);
-        maxIdleMinutes = maxSeconds/60
+        maxIdleMinutes = maxSeconds / 60
     }
 }
 
@@ -36,6 +76,6 @@ function runIdleLogoutOnThisPage() {
 $(document).ready(function () {
     getVariables();
     if (runIdleLogoutOnThisPage()) {
-        startWatchers()
+        startIdleTimer()
     }
 });
