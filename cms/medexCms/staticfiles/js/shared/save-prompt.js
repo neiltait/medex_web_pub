ba60b6fbd6682eb654ddbac7f4cc0b8a592e2105
Tom Ridd <twridd@gmail.com>
2019-07-15 15:35:24 +0100

    add to patient details and medical team screens

--- cms/medexCms/staticfiles/js/shared/save-prompt.js
+++ cms/medexCms/staticfiles/js/shared/save-prompt.js
@@ -8,21 +8,30 @@ var SavePromptForm = function (form) {
 
 SavePromptForm.prototype = {
     setup: function () {
-        this.content = this.form.serialize()
-        that = this;
-        this.form.submit(function (e) {
-            that.submittedAsForm = true;
-        });
-        this.bindWindow();
+        if (this.form.length > 0) {
+            this.content = this.serializeIncludingDisabled(this.form);
+            var that = this;
+            this.form.submit(function (e) {
+                that.submittedAsForm = true;
+            });
+            this.bindWindow();
+        }
     },
     bindWindow: function () {
         var that = this;
         window.onbeforeunload = function (e) {
-            var newData = that.form.serialize();
+            var newData = that.serializeIncludingDisabled(that.form);
             if (!that.submittedAsForm && (newData !== that.content)) {
                 e.returnValue = that.prompt;
                 return that.prompt
             }
         }
+    },
+    serializeIncludingDisabled: function (form) {
+        var disabled = form.find(':input:disabled').removeAttr('disabled');
+        var serialized = this.form.serialize();
+        disabled.attr('disabled', 'disabled');
+        return serialized
+
     }
 };
