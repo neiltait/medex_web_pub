9d78f076da42ee8412d6d18e60fe0ad402d17f38
Tom Ridd <twridd@gmail.com>
2019-08-28 13:29:03 +0100

    fix issue by disabling submit buttons after first click

---
+++ cms/medexCms/staticfiles/js/examinations/partials/disabling-form.js
@@ -0,0 +1,29 @@
+/*
+    Ensures all forms with the class 'disabling-form' only submit once but disabling all submit buttons on form
+    submission
+ */
+
+(function ($) {
+    var SelfDisablingFormSet = function (forms) {
+        this.forms = forms;
+        this.setup()
+    };
+
+    SelfDisablingFormSet.prototype = {
+        setup: function () {
+            this.forms.one('submit', function () {
+                $(this).find('input[type="submit"]').attr('disabled', 'disabled');
+            });
+        }
+    };
+
+    function init() {
+        var disablingForms = $('.disabling-form');
+        new SelfDisablingForm(disablingForms);
+    }
+
+    $(document).on('page:load', init);
+    $(init)
+}(jQuery));
+
+
