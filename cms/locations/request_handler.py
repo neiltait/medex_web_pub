5363358035fb3e7c0611fcacae95c16d85b5367b
Tom Ridd <twridd@gmail.com>
2019-09-04 12:33:00 +0100

    refactor locations handler to make more DRY

--- cms/locations/request_handler.py
+++ cms/locations/request_handler.py
@@ -8,23 +8,21 @@ def get_locations_list(auth_token, limit_to_me_offices=True):
     if settings.LOCAL:
         return LocationsMocks.get_trust_location_list()
     else:
-        if limit_to_me_offices:
-            return MedexRequest.get(auth_token, "%s/locations?OnlyMEOffices=true" % settings.API_URL).json()[
-                'locations']
-        else:
-            return MedexRequest.get(auth_token, "%s/locations" % settings.API_URL).json()['locations']
+        return __get_filtered_locations_list(auth_token, permitted_locations_only=False,
+                                             limit_to_me_offices=limit_to_me_offices)
 
 
 def get_permitted_locations_list(auth_token, limit_to_me_offices=True):
     if settings.LOCAL:
         return LocationsMocks.get_trust_location_list()
     else:
-        query_params = {
-            "AccessOnly": True
-        }
-        if limit_to_me_offices:
-            return \
-            MedexRequest.get(auth_token,
-                             "%s/locations?OnlyMEOffices=true" % settings.API_URL, query_params).json()['locations']
-        else:
-            return MedexRequest.get(auth_token, "%s/locations" % settings.API_URL, query_params).json()['locations']
+        return __get_filtered_locations_list(auth_token, permitted_locations_only=True,
+                                             limit_to_me_offices=limit_to_me_offices)
+
+
+def __get_filtered_locations_list(auth_token, permitted_locations_only, limit_to_me_offices):
+    query_params = {
+        "AccessOnly": permitted_locations_only,
+        "OnlyMEOffices": limit_to_me_offices
+    }
+    return MedexRequest.get(auth_token, "%s/locations" % settings.API_URL, query_params).json()['locations']
