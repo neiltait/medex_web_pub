b0b55c9cf7aa6c69a55428170562c1fba7cd3519
Noelle Legrain <noelle.legrain@gmail.com>
2019-09-11 12:08:13 +0100

    creating case continues to patient details

--- cms/medexCms/staticfiles/js/shared/disable-submit.js
+++ cms/medexCms/staticfiles/js/shared/disable-submit.js
@@ -1,27 +1,19 @@
 /*
     Ensures all forms with the class 'disabling-form' only submit once but disabling all submit buttons on form
     submission
- */
+*/
+
 
 (function ($) {
-    var SelfDisablingFormSet = function (forms) {
-        this.forms = forms;
-        this.setup()
-    };
 
-    SelfDisablingFormSet.prototype = {
-        setup: function () {
-            this.forms.one('submit', function () {
-                $(this).find('input[type="submit"]').attr('disabled', 'disabled');
-            });
-        }
-    };
-
-    function init() {
-        var disablingForms = $('.disabling-form');
-        new SelfDisablingFormSet(disablingForms);
+    function disableSubmit() {
+        $('#examination__create--form').one('submit', function (event) {
+                event.preventDefault();
+                $('input[type="submit"]').submit();
+        });
     }
 
-    $(document).on('page:load', init);
-    $(init)
+    $(document).on('page:load', disableSubmit);
+    $(disableSubmit)
 }(jQuery));
+
