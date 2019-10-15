7d6fbfc59f76031bf212c9caf26fab051dd718dc
mwnicks <matthew.nicks@methods.co.uk>
2019-10-15 10:52:45 +0100

    fix linter errors

--- cms/locations/request_handler.py
+++ cms/locations/request_handler.py
@@ -10,7 +10,7 @@ def get_locations_list(auth_token, limit_to_me_offices=True):
     else:
         return __get_filtered_locations_list(auth_token, permitted_locations_only=False,
                                              limit_to_me_offices=limit_to_me_offices,
-                                             create_examination_permitted_locations_only=False)
+                                             create_examination_locations=False)
 
 
 def get_permitted_locations_list(auth_token, limit_to_me_offices=True):
@@ -19,7 +19,8 @@ def get_permitted_locations_list(auth_token, limit_to_me_offices=True):
     else:
         return __get_filtered_locations_list(auth_token, permitted_locations_only=True,
                                              limit_to_me_offices=limit_to_me_offices,
-                                             create_examination_permitted_locations_only=False)
+                                             create_examination_locations=False)
+
 
 def get_create_examination_permitted_locations_list(auth_token, limit_to_me_offices=True):
     if settings.LOCAL:
@@ -27,12 +28,14 @@ def get_create_examination_permitted_locations_list(auth_token, limit_to_me_offi
     else:
         return __get_filtered_locations_list(auth_token, permitted_locations_only=False,
                                              limit_to_me_offices=limit_to_me_offices,
-                                             create_examination_permitted_locations_only=True)
+                                             create_examination_locations=True)
 
-def __get_filtered_locations_list(auth_token, permitted_locations_only, limit_to_me_offices, create_examination_permitted_locations_only):
+
+def __get_filtered_locations_list(auth_token, permitted_locations_only, limit_to_me_offices,
+                                  create_examination_locations):
     query_params = {
         "AccessOnly": permitted_locations_only,
         "OnlyMEOffices": limit_to_me_offices,
-        "CreateExaminationOnly" : create_examination_permitted_locations_only,
+        "CreateExaminationOnly": create_examination_locations,
     }
     return MedexRequest.get(auth_token, "%s/locations" % settings.API_URL, query_params).json()['locations']
